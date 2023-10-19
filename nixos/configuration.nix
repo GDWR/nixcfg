4b948fe0cf51e817df8b1076626b6a615526d391
GDWR <gregory.dwr@gmail.com>
2023-10-19 11:50:02 +0100

    Use updated options for openssh config

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -86,9 +86,11 @@
   services.openssh = {
     enable = true;
     # Forbid root login through SSH.
-    permitRootLogin = "no";
-    # Use keys only. Remove if you want to SSH using password (not recommended)
-    passwordAuthentication = false;
+    settings = {
+      PermitRootLogin = "no";
+      # Use keys only. Remove if you want to SSH using password (not recommended)
+      PasswordAuthentication = false;
+    };
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
