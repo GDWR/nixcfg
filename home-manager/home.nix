20466d189456a40079d4d81e17cc2be5d3f61725
GDWR <gregory.dwr@gmail.com>
2023-10-20 17:11:47 +0100

    Add background for darkmode

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -69,9 +69,14 @@
   dconf.settings = {
     "org/gnome/desktop/background" = {
         "picture-uri" = "/home/gdwr/.background";
+	"picture-uri-dark" = "/home/gdwr/.background";
     };
     "org/gnome/desktop/screensaver" = {
         "picture-uri" = "/home/gdwr/.background";
+	"picture-uri-dark" = "/home/gdwr/.background";
+    };
+    "org/gnome/desktop/interface" = {
+      "color-scheme" = "prefer-dark";
     };
   };
   home.file.".background".source = ./backgrounds/firewatch.jpg;
