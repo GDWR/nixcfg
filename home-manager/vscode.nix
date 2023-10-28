a12a170d87b10634f79ae4576adf212df2a80735
GDWR <gregory.dwr@gmail.com>
2023-10-28 15:57:14 +0100

    Add vscode settings

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -8,9 +8,24 @@
       bbenoist.nix
       vscodevim.vim
     ];
+    # https://nix-community.github.io/home-manager/options.html#opt-programs.vscode.userSettings
     userSettings = {
       "files.autoSave" = "onFocusChange";
       "window.titleBarStyle" = "custom";
+      "git.autofetch" = true;
+      "editor.minimap.enabled" = false;
+      "editor.smoothScrolling" = true;
+      "editor.mouseWheelZoom" = true;
+      "editor.fontLigatures" = true;
+      "explorer.autoReveal" = true;
+      "debug.toolBarLocation" = "docked";
+
+      "terminal.integrated.defaultProfile.linux" = "nu";
+      "terminal.integrated.profiles.linux" = {
+        "nu" = {
+          "path" = "/home/gdwr/.nix-profile/bin/nu";
+        };
+      };
     };
   };
 }
