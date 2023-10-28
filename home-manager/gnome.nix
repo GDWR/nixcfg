1f74430888764b52eae6970a171c279d892f0cd5
GDWR <gregory.dwr@gmail.com>
2023-10-28 21:13:43 +0100

    Add ctrl+alt+t terminal keybind

--- home-manager/gnome.nix
+++ home-manager/gnome.nix
@@ -4,14 +4,22 @@
   dconf.settings = {
     "org/gnome/desktop/background" = {
         "picture-uri" = "/home/gdwr/.background";
-	"picture-uri-dark" = "/home/gdwr/.background";
+	      "picture-uri-dark" = "/home/gdwr/.background";
     };
     "org/gnome/desktop/screensaver" = {
         "picture-uri" = "/home/gdwr/.background";
-	"picture-uri-dark" = "/home/gdwr/.background";
+	      "picture-uri-dark" = "/home/gdwr/.background";
     };
     "org/gnome/desktop/interface" = {
       "color-scheme" = "prefer-dark";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      "custom-keybindings" = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybinding/terminal/"];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybinding/terminal" = {
+      "binding" = "<Control><Alt>t";
+      "command" = "kgx";
+      "name" = "terminal";
     };
   };
 }
