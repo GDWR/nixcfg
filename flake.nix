f2c94aaf8f1b21816ba6f3953207704d617095bb
GDWR <gregory.dwr@gmail.com>
2023-11-19 15:44:59 +0000

    Remove unused flake-utils

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }@inputs: {
+  outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs;};
@@ -20,6 +20,6 @@
     packages = {
       x86_64-linux.krisp-patch = nixpkgs.legacyPackages.x86_64-linux.callPackage ./packages/krisp-patch { pkgs = nixpkgs.legacyPackages.x86_64-linux; };
       aarch64-linux.krisp-patch = nixpkgs.legacyPackages.aarch64-linux.callPackage ./packages/krisp-patch { pkgs = nixpkgs.legacyPackages.aarch64-linux; };
-    };
+          };
   };
 }
