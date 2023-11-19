d4e6d140fae18bce9aa2f5c458fe2f8c29749b26
GDWR <gregory.dwr@gmail.com>
2023-11-19 15:48:26 +0000

    Fix formatting

--- flake.nix
+++ flake.nix
@@ -20,6 +20,6 @@
     packages = {
       x86_64-linux.krisp-patch = nixpkgs.legacyPackages.x86_64-linux.callPackage ./packages/krisp-patch { pkgs = nixpkgs.legacyPackages.x86_64-linux; };
       aarch64-linux.krisp-patch = nixpkgs.legacyPackages.aarch64-linux.callPackage ./packages/krisp-patch { pkgs = nixpkgs.legacyPackages.aarch64-linux; };
-          };
+    };
   };
 }
