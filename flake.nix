fd7fb106914ddfdd30b7892204798f3a40f60d47
GDWR <gregory.dwr@gmail.com>
2024-02-24 10:26:44 +0000

    Remove spacetimedb

--- flake.nix
+++ flake.nix
@@ -24,19 +24,6 @@
     packages = {
       x86_64-linux.krisp-patch = nixpkgs.legacyPackages.x86_64-linux.callPackage ./packages/krisp-patch { pkgs = nixpkgs.legacyPackages.x86_64-linux; };
       aarch64-linux.krisp-patch = nixpkgs.legacyPackages.aarch64-linux.callPackage ./packages/krisp-patch { pkgs = nixpkgs.legacyPackages.aarch64-linux; };
-      x86_64-linux.spacetimedb = nixpkgs.legacyPackages.x86_64-linux.callPackage ./packages/spacetimedb { pkgs = nixpkgs.legacyPackages.x86_64-linux; };
-      aarch64-linux.spacetimedb = nixpkgs.legacyPackages.aarch64-linux.callPackage ./packages/spacetimedb { pkgs = nixpkgs.legacyPackages.aarch64-linux; };
-    };
-
-    apps = {
-      x86_64-linux.spacetimedb = {
-        type = "app";
-        program = "${self.packages.x86_64-linux.spacetimedb}/bin/spacetime";
-      };
-      aarch64-linux.spacetimedb = {
-        type = "app";
-        program = "${self.packages.aarch64-linux.spacetimedb}/bin/spacetime";
-      };
     };
   };
 }
