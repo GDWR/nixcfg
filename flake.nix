bf979c578f224f5b37d1a08e4744ef45c48d7024
GDWR <gregory.dwr@gmail.com>
2024-03-05 11:01:19 +0000

    Remove nixpkgs2311 input

--- flake.nix
+++ flake.nix
@@ -3,8 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs2311.url = "github:nixos/nixpkgs/nixos-23.11";
-
+  
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,7 +18,7 @@
     };
     jetpack = {
       url = "github:anduril/jetpack-nixos";
-      inputs.nixpkgs.follows = "nixpkgs2311";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
