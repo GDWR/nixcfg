8d20e1d2e1322e3bf8ea4d6cfb7dd6c78b4fcfe0
GDWR <gregory.dwr@gmail.com>
2024-03-10 11:23:14 +0000

    Add neovide and nvim config

--- home-manager/neovim.nix
+++ home-manager/neovim.nix
@@ -1,8 +1,18 @@
-{ ... }: {
+{ pkgs, ... }: {
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     vimAlias = true;
     viAlias = true;
+    extraLuaConfig = ''
+      vim.o.number = true
+      vim.o.relativenumber = true
+
+      vim.o.guifont = "JetBrains Mono:h14"
+    '';
   };
+
+  home.packages = with pkgs; [
+    neovide
+  ];
 }
