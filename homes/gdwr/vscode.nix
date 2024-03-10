6b50871c1f9cbedde71d571570c96671a240c81c
GDWR <gregory.dwr@gmail.com>
2024-03-10 15:43:46 +0000

    vscode immutable extension directory

--- homes/gdwr/vscode.nix
+++ homes/gdwr/vscode.nix
@@ -1,6 +1,9 @@
 { pkgs, ... }: {
   programs.vscode = {
     enable = true;
+    enableUpdateCheck = false;
+    enableExtensionUpdateCheck = false;
+    mutableExtensionsDir = false;
 
     extensions = with pkgs.vscode-extensions; [
       bbenoist.nix
