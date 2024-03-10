cf172ee34b2f70b629a192b63b233656d8e5f4c3
GDWR <gregory.dwr@gmail.com>
2024-03-10 13:02:39 +0000

    Explicitly enable dconf

--- homes/gdwr/gnome.nix
+++ homes/gdwr/gnome.nix
@@ -2,26 +2,29 @@
 { ... }: {
   home.file.".background".source = ../../assets/firewatch.jpg;
   home.file.".face".source = ../../assets/gdwr.png;
-  dconf.settings = {
-    "org/gnome/desktop/background" = {
-      "picture-uri" = "/home/gdwr/.background";
-      "picture-uri-dark" = "/home/gdwr/.background";
-    };
-    "org/gnome/desktop/screensaver" = {
-      "picture-uri" = "/home/gdwr/.background";
-      "picture-uri-dark" = "/home/gdwr/.background";
-    };
-    "org/gnome/desktop/interface" = { "color-scheme" = "prefer-dark"; };
-    "org/gnome/settings-daemon/plugins/media-keys" = {
-      "custom-keybindings" = [
-        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybinding/terminal/"
-      ];
-    };
-    "org/gnome/settings-daemon/plugins/media-keys/custom-keybinding/terminal" =
-      {
-        "binding" = "<Control><Alt>t";
-        "command" = "kgx";
-        "name" = "terminal";
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/desktop/background" = {
+        "picture-uri" = "/home/gdwr/.background";
+        "picture-uri-dark" = "/home/gdwr/.background";
       };
+      "org/gnome/desktop/screensaver" = {
+        "picture-uri" = "/home/gdwr/.background";
+        "picture-uri-dark" = "/home/gdwr/.background";
+      };
+      "org/gnome/desktop/interface" = { "color-scheme" = "prefer-dark"; };
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        "custom-keybindings" = [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybinding/terminal/"
+        ];
+      };
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybinding/terminal" =
+        {
+          "binding" = "<Control><Alt>t";
+          "command" = "kgx";
+          "name" = "terminal";
+        };
+    };
   };
 }
