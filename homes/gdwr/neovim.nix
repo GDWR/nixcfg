efb6276ddfa83347bf8d0cd8e5bbc32d25792df1
GDWR <gregory.dwr@gmail.com>
2024-03-17 09:21:29 +0000

    Update neovim config

--- homes/gdwr/neovim.nix
+++ homes/gdwr/neovim.nix
@@ -1,20 +1,31 @@
 { pkgs, ... }: {
+  home.packages = with pkgs; [  neovide  ];
+
   programs.neovim = {
     enable = true;
     defaultEditor = true;
     vimAlias = true;
     viAlias = true;
     plugins = with pkgs.vimPlugins; [
-      vim-surround
       neo-tree-nvim
+      telescope-nvim
+
+      nvim-lspconfig
     ];
     extraLuaConfig = ''
+      vim.g.mapleader = " "
       vim.o.number = true
-      vim.o.relativenumber = true
+      vim.o.relativenumber = true 
 
-      vim.o.guifont = "JetBrains Mono:h14"
+      -- General keybinds
+      vim.keymap.set("n", "<leader>c", "<cmd>bd!<cr>")
+
+      -- Configure Telescope
+      vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+      vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+
+      -- Configure Neotree
+      vim.keymap.set("n", "<leader>t", "<cmd>Neotree toggle<cr>")
     '';
   };
-
-  home.packages = with pkgs; [ neovide ];
 }
