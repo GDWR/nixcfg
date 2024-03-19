ea8cafb934f218c38321277d2e5ec7caafcd626f
GDWR <gregory.dwr@gmail.com>
2024-03-19 20:00:26 +0000

    Remove unused configuration

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -10,21 +10,9 @@
   nix = {
     optimise.automatic = true;
 
-    # This will add each flake input as a registry
-    # To make nix3 commands consistent with your flake
-    registry = lib.mapAttrs (_: value: { flake = value; }) inputs;
-
-    # This will additionally add your inputs to the system's legacy channels
-    # Making legacy nix commands consistent as well, awesome!
-    nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}")
-      config.nix.registry;
-
     settings = {
-      # Enable flakes and new 'nix' command
       experimental-features = "nix-command flakes";
-      # Deduplicate and optimize nix store
       auto-optimise-store = true;
-
       trusted-users = [ "root" "gdwr" ];
     };
   };
@@ -90,8 +78,6 @@
     pkgs.pinentry
     pkgs.stdenv.cc.cc
     inputs.agenix.packages.x86_64-linux.default
-
-    pkgs.gnomeExtensions.tophat
   ];
 
   # Exclude Default Gnome Apps
