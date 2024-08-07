6522362579a0c015ee386002e352c9e5c56a90af
GDWR <gregory.dwr@gmail.com>
2024-08-07 12:00:07 +0100

    Expand tabs in neovim

--- homes/gdwr/neovim.nix
+++ homes/gdwr/neovim.nix
@@ -26,6 +26,7 @@
       vim.o.relativenumber = true 
       vim.o.termguicolors = true
       vim.o.tabstop = 4
+      vim.o.expandtab = true
 
       -- Misc Keymaps
       vim.keymap.set("n", "<leader>c", "<cmd>bd!<cr>")
