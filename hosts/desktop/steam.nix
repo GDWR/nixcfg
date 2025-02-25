815a095d4ade32c6e8d0c79ec7d3fad9dce04b00
GDWR <gregory.dwr@gmail.com>
2025-02-25 23:12:56 +0000

    Correct typo

--- hosts/desktop/steam.nix
+++ hosts/desktop/steam.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }: {
   programs.steam.enable = true;
-  programs.steam.protrontricks.enable = true;
+  programs.steam.protontricks.enable = true;
   programs.steam.gamescopeSession.enable = true;
   programs.gamemode.enable = true;
 
