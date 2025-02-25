4d6334171bb1b4a292a11efcc6a743fc999bbd29
GDWR <gregory.dwr@gmail.com>
2025-02-25 23:36:12 +0000

    Remove duplicate nvidia-container-toolkit declaration

--- hosts/desktop/hardware-configuration.nix
+++ hosts/desktop/hardware-configuration.nix
@@ -39,7 +39,6 @@
 
   hardware = {
     cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
-    nvidia-container-toolkit.enable = true;
     nvidia = {
       modesetting.enable = true;
       powerManagement.enable = false;
