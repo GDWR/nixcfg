a55c7fbbddfad42b3db83af3b6b403cbc1090cf5
GDWR <gregory.dwr@gmail.com>
2025-03-01 10:07:26 +0000

    Remove configuration that is now the default https://github.com/nix-community/home-manager/issues/5452

--- homes/gdwr/default.nix
+++ homes/gdwr/default.nix
@@ -11,6 +11,7 @@
   ];
 
   nixpkgs.config = {
+
     allowUnfree = true;
     overlays = [
       (import ./overlays/vesktop.nix { inherit pkgs; })
@@ -39,9 +40,6 @@
   };
 
   programs.browserpass.enable = true;
-
-  # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   home.stateVersion = "23.05";
