686d664a270be8cffc8cab363fc2d232929b5038
GDWR <gregory.dwr@gmail.com>
2025-06-12 22:48:31 +0100

    Correct the desktop exec entry

--- homes/gdwr/packages/awakened-poe-trade/default.nix
+++ homes/gdwr/packages/awakened-poe-trade/default.nix
@@ -17,6 +17,9 @@ appimageTools.wrapType2 {
 
   extraInstallCommands = ''
     install -m 444 -D ${appimageContents}/awakened-poe-trade.desktop $out/share/applications/${pname}.desktop
+    substituteInPlace $out/share/applications/awakened-poe-trade.desktop \
+      --replace "Exec=AppRun --sandbox %U" "Exec=awakened-poe-trade %U"
+
     install -m 444 -D ${appimageContents}/awakened-poe-trade.png $out/share/icons/hicolor/128x128/apps/${pname}.png
   '';
 
