5fa0cb0d5639008f08ead723dad62c670144bc3c
GDWR <gregory.dwr@gmail.com>
2025-06-21 17:34:13 +0100

    Pin pob version

--- homes/gdwr/overlays/path-of-building.nix
+++ homes/gdwr/overlays/path-of-building.nix
@@ -1,6 +1,11 @@
 { system, ... }:
-(final: prev: {
-  path-of-building = prev.path-of-building.overrideAttrs (old: {
+(final: prev: let
+  nixpkgs = import (builtins.fetchTarball {
+    url = "https://github.com/nixos/nixpkgs/archive/0feafdbc84139a414f5d46a11cf91fc4fcd85ffb.tar.gz";
+    sha256 = "sha256:15lplxgcqz9bdf8qa7ri63jy7zakm7ldrmqmfmjrp37ig752rs4x";
+  }) { inherit system; };
+in {
+  path-of-building = nixpkgs.path-of-building.overrideAttrs (old: {
     postFixup = ''
       substituteInPlace $out/share/applications/path-of-building.desktop \
         --replace "Exec=pobfrontend %U" "Exec=env WAYLAND_DISPLAY=wayland-0 pobfrontend %U"
