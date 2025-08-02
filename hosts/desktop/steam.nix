5e664f2a1cff2c945c22af59b0a10ac5312e2916
GDWR <gregory.dwr@gmail.com>
2025-08-02 13:13:32 +0100

    Disable screensaver inhibiter

--- hosts/desktop/steam.nix
+++ hosts/desktop/steam.nix
@@ -3,6 +3,7 @@
   programs.steam.protontricks.enable = true;
   programs.steam.gamescopeSession.enable = true;
   programs.gamemode.enable = true;
+  programs.gamemode.settings.general.inhibit_screensaver = 0;
 
   environment.systemPackages = with pkgs; [
     mangohud
