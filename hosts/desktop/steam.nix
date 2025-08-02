d0f287097a7295d31ae540554a1669555a64dc8e
GDWR <gregory.dwr@gmail.com>
2025-08-02 13:15:06 +0100

    Add proton-ge-bin

--- hosts/desktop/steam.nix
+++ hosts/desktop/steam.nix
@@ -2,6 +2,8 @@
   programs.steam.enable = true;
   programs.steam.protontricks.enable = true;
   programs.steam.gamescopeSession.enable = true;
+  programs.steam.extraCompatPackages = [ pkgs.proton-ge-bin ];
+
   programs.gamemode.enable = true;
   programs.gamemode.settings.general.inhibit_screensaver = 0;
 
