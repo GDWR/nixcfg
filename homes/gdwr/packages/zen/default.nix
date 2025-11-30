f37bddf2f8183f667bfe199d8dff728447de2bd5
GDWR <gregory.dwr@gmail.com>
2025-11-30 11:11:26 +0000

    Update nix flake lock and zen

--- homes/gdwr/packages/zen/default.nix
+++ homes/gdwr/packages/zen/default.nix
@@ -1,11 +1,11 @@
 { appimageTools, fetchurl, ... }:
 let
   pname = "zen";
-  version = "1.17.8b";
+  version = "1.17.11b";
 
   src = fetchurl {
-    url = "https://github.com/zen-browser/desktop/releases/download/1.17.8b/zen-x86_64.AppImage";
-    sha256 = "sha256-fjDW7K8fq/ekGuRLaCHK7RWuKe+EbhqfySOldo53EGw=";
+    url = "https://github.com/zen-browser/desktop/releases/download/1.17.11b/zen-x86_64.AppImage";
+    sha256 = "sha256-OhEAP/CgCYy5kEYP7QABAHN/aXlE5IbOM0cYjX3N0a8=";
   };
 
   appimageContents = appimageTools.extract {
