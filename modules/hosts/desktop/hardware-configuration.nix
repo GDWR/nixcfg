ad31105919bbb0761ed551415c04a0d9692d82ae
GDWR <gregory.dwr@gmail.com>
2026-05-10 17:20:12 +0100

    Update flake lock and remove nvidia container toolkit

--- modules/hosts/desktop/hardware-configuration.nix
+++ modules/hosts/desktop/hardware-configuration.nix
@@ -2,9 +2,6 @@
   flake.nixosModules.desktopHardware = { config, lib, modulesPath, ... }: {
     imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
-    hardware.nvidia-container-toolkit.suppressNvidiaDriverAssertion = true;
-    hardware.nvidia-container-toolkit.enable = true;
-
     boot.initrd.availableKernelModules =
       [ "nvme" "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
     boot.kernelModules = [ "kvm-amd" ];
